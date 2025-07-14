a71ff9af7207c2069ddd616db0cabecfea3f8f62
artpereverzev <artpereverzev@gmail.com>
2025-07-14 17:08:50 +0300

    - Added SplashView - Added OAuth impl - Using Unsplash API

---
+++ ImageFeed/Extensions/UIColor+ImageFeed.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+ImageFeed.swift
+//  ImageFeed
+//
+//  Created by Artem Pereverzev on 13.07.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - Main App Colors
+    static let ypBlack = UIColor(red: 0.102, green: 0.106, blue: 0.133, alpha: 1)
+    static let ypLightGray = UIColor(red: 0.682, green: 0.686, blue: 0.706, alpha: 1)
+    
+}
